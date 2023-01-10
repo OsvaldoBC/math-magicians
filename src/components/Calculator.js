8e1a63e04c8d63c0ec8bd4a3544df28a21f98129
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2023-01-10 16:17:05 -0500

    Delete all the boilerplate and create the folder components

---
+++ src/components/Calculator.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Calculator.css';
+
+class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = ''
+      }
+    render() {
+  return (
+    <div>
+    <h1>Hello React!</h1>
+    </div>
+  );
+    }
+  }
+
+export default Calculator;
