9464de80da7e76dde5a98e3c28c6bb66441e85e3
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2023-01-11 16:36:23 -0500

    Add linter Styles

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -22,22 +22,22 @@ class Calculator extends React.Component {
       <section className="calculator-container flex-row">
         <div className="output-container flex-row">
           <div className="output">
-          <p>{total}</p>
-          <p>{operation}</p>
-          <p>{next}</p>
-        </div>
+            <p>{total}</p>
+            <p>{operation}</p>
+            <p>{next}</p>
+          </div>
         </div>
         <div className="gray-container flex-row">
-          <button type="button" name="AC" onClick={this.handleClick}  className="gray-button">
+          <button type="button" name="AC" onClick={this.handleClick} className="gray-button">
             AC
           </button>
           <button type="button" name="+/-" onClick={this.handleClick} className="gray-button">
             +/-
           </button>
-          <button type="button" name="%" onClick={this.handleClick}  className="gray-button">
+          <button type="button" name="%" onClick={this.handleClick} className="gray-button">
             %
           </button>
-          <button type="button"  name="7" onClick={this.handleClick} className="gray-button">
+          <button type="button" name="7" onClick={this.handleClick} className="gray-button">
             7
           </button>
           <button type="button" name="8" onClick={this.handleClick} className="gray-button">
@@ -49,7 +49,7 @@ class Calculator extends React.Component {
           <button type="button" name="4" onClick={this.handleClick} className="gray-button">
             4
           </button>
-          <button type="button" name="5" onClick={this.handleClick}  className="gray-button">
+          <button type="button" name="5" onClick={this.handleClick} className="gray-button">
             5
           </button>
           <button type="button" name="6" onClick={this.handleClick} className="gray-button">
@@ -72,7 +72,7 @@ class Calculator extends React.Component {
           </button>
         </div>
         <div className="orange-container flex-col">
-          <button type="button"name="÷" onClick={this.handleClick} className="orange-button">
+          <button type="button" name="÷" onClick={this.handleClick} className="orange-button">
             &#247;
           </button>
           <button type="button" name="x" onClick={this.handleClick} className="orange-button">
@@ -84,7 +84,7 @@ class Calculator extends React.Component {
           <button type="button" name="+" onClick={this.handleClick} className="orange-button">
             +
           </button>
-          <button type="button" name="=" onClick={this.handleClick}  className="orange-button">
+          <button type="button" name="=" onClick={this.handleClick} className="orange-button">
             =
           </button>
         </div>
